806b461ef4be8f801ea28ef0dcb0442c8e7c35ec
DerSchmaik <aiko.zimmermann@gmx.net>
2022-02-23 18:00:09 +0100

    optimizing deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["de"],
+    defaultLocale: "de",
+  },
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+
+module.exports = withBundleAnalyzer(nextConfig)
