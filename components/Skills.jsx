a9c647020b9d35f6a10beb0313208dca7d3d4481
DerSchmaik <aiko.zimmermann@gmx.net>
2022-02-24 13:58:13 +0100

    Optimizations (Center Buttons, COlor Tint)

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -9,91 +9,91 @@ export default function Skills({ IconSize, spacingH, spacingV }) {
   return(<>
     <Flex flexWrap='wrap'>
       <Tooltip label="Javascript" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={DiJavascript1} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={DiJavascript1} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Typescript" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={SiTypescript} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={SiTypescript} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Python" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={SiPython} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={SiPython} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="C#" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={SiCsharp} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={SiCsharp} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Vue.js" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={FaVuejs} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={FaVuejs} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Nuxt.js" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={SiNuxtdotjs} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={SiNuxtdotjs} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="React" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={FaReact} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={FaReact} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Next.js" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={SiNextdotjs} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={SiNextdotjs} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Vuetify" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={SiVuetify} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={SiVuetify} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Chakra UI" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={SiChakraui} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={SiChakraui} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Bootstrap" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={FaBootstrap} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={FaBootstrap} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Firebase" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={SiFirebase} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={SiFirebase} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Git" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={DiGit} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={DiGit} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Github" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={FaGithub} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={FaGithub} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="NPM" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={DiNpm} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={DiNpm} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="pypi" placement='top-start' shouldWrapChildren closeOnClick={false}>
-      <Icon as={SiPypi} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+      <Icon as={SiPypi} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Express" placement='top-start' shouldWrapChildren closeOnClick={false}>
-      <Icon as={SiExpress} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+      <Icon as={SiExpress} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="MongoDB" placement='top-start' shouldWrapChildren closeOnClick={false}>
-      <Icon as={SiMongodb} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+      <Icon as={SiMongodb} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Unity" placement='top-start' shouldWrapChildren closeOnClick={false}>
-      <Icon as={DiUnitySmall} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+      <Icon as={DiUnitySmall} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Raspberry Pi" placement='top-start' shouldWrapChildren closeOnClick={false}>
-      <Icon as={SiRaspberrypi} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+      <Icon as={SiRaspberrypi} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Ubuntu" placement='top-start' shouldWrapChildren closeOnClick={false}>
-      <Icon as={DiUbuntu} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+      <Icon as={DiUbuntu} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
 
       <Tooltip label="Windows" placement='top-start' shouldWrapChildren closeOnClick={false}>
-        <Icon as={DiWindows} w={IconSize} h={IconSize} marginRight={spacingH} marginBottom={spacingV}/>
+        <Icon as={DiWindows} w={IconSize} h={IconSize} marginRight={spacingH / 2} marginLeft={spacingH / 2}  marginBottom={spacingV}/>
       </Tooltip>
     </Flex>
 
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,7 @@
 import '../styles/globals.css'
-import { ChakraProvider, extendTheme, useColorModeValue } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Head from 'next/head'
 
 const theme = extendTheme({
   initialColorMode: 'dark',
@@ -11,28 +10,7 @@ const theme = extendTheme({
 
 
 function MyApp({ Component, pageProps }) {
-  const favicon = useColorModeValue('faviconL.ico', 'faviconD.ico');
-
   return (<>
-    <Head>
-      <link rel="icon" href={`/${favicon}`} />
-
-      <meta name="title" content="Schmaik.net" />
-      <meta name="description" content="Hallo Welt! Ich bin Aiko aka. Schmaik und ein 15 Jähriger entwickler. Hier findest du alle meine Projekte und Infos." />
-      <meta name="keywords" content="schmaik, aiko, developer, full stack, teenager, projekte, fly or die"/>
-      <meta name="image" content="https://i.imgur.com/EuBwo7m.jpg" />
-      <meta name="robots" content="index, follow"/>
-      <meta name="language" content="German"/>
-      <meta name="revisit-after" content="1 days"/>
-
-      <meta property="og:title" content="Schmaik.net" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://schmaik.net" />
-      <meta property="og:image" content="https://i.imgur.com/EuBwo7m.jpg" />
-      <meta property="og:description" content="Hallo Welt! Ich bin Aiko aka. Schmaik und ein 15 Jähriger entwickler. Hier findest du alle meine Projekte und Infos." />
-      <meta name="theme-color" content="#FF0000" />
-    </Head>
-
     <ChakraProvider theme={theme}>
       <Header />
       <Component {...pageProps} />
