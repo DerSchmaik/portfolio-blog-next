86135748f303dd2313698da5e55f63ae372a37ad
DerSchmaik <aiko.zimmermann@gmx.net>
2022-03-01 20:56:23 +0100

    fix Bug

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -7,6 +7,45 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { foundation, atomOneDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 export default function articlePage({ post }) {
+  // Styling of the blog post
+  const ptComponents = {
+    marks: {
+      link: ({children, value}) => {
+        return (
+          <Link href={value.href} isExternal color="teal">
+            {children}
+          </Link>
+        )
+      },
+    },
+    types: {
+      code: ({ value }) => {
+        const style = useColorModeValue(foundation, atomOneDark)
+        const bg = useColorModeValue('#eee', '#282a34')
+  
+        if (!value || !value.code) { return null }
+        return(
+          <Box padding={3} bgColor={bg} borderRadius={6}>
+            <SyntaxHighlighter language={value.language || 'text'} style={style}>
+              {value.code}
+            </SyntaxHighlighter>
+          </Box>
+        )
+      }
+    },
+    block: {
+      normal: ({ children }) => <Box marginTop={4} marginBottom={4}>{children}</Box>,
+      h1: ({children}) => <><Heading as="h1" size="xl" marginTop="30px">{children}</Heading> <Divider /> </>,
+      h2: ({children}) => <><Heading as="h2" size="lg" marginTop="30px">{children}</Heading> <Divider /></>,
+      h3: ({children}) => <Heading as="h3" size="md" marginTop="30px">{children}</Heading>,
+      h4: ({children}) => <Heading as="h4" size="sm" marginTop="30px">{children}</Heading>,
+    },
+    list: {
+      bullet: ({ children }) => <Box as="ul" margin={5} >{children}</Box>,
+      number: ({ children }) => <Box as="ol" margin={5} >{children}</Box>,
+    },
+  }
+
   return(<>
     <Metadata
       pageTitle={post.title}
@@ -35,44 +74,6 @@ export default function articlePage({ post }) {
   </>)
 }
 
-const ptComponents = {
-  marks: {
-    link: ({children, value}) => {
-      const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined
-      return (
-        <Link href={value.href} isExternal color="teal">
-          {children}
-        </Link>
-      )
-    },
-  },
-  types: {
-    code: ({ value }) => {
-      const style = useColorModeValue(foundation, atomOneDark)
-      const bg = useColorModeValue('#eee', '#282a34')
-
-      if (!value || !value.code) { return null }
-      return(
-        <Box padding={3} bgColor={bg} borderRadius={6}>
-          <SyntaxHighlighter language={value.language || 'text'} style={style}>
-            {value.code}
-          </SyntaxHighlighter>
-        </Box>
-      )
-    }
-  },
-  block: {
-    normal: ({ children }) => <Box marginTop={4} marginBottom={4}>{children}</Box>,
-    h1: ({children}) => <><Heading as="h1" size="xl" marginTop="30px">{children}</Heading> <Divider /> </>,
-    h2: ({children}) => <><Heading as="h2" size="lg" marginTop="30px">{children}</Heading> <Divider /></>,
-    h3: ({children}) => <Heading as="h3" size="md" marginTop="30px">{children}</Heading>,
-    h4: ({children}) => <Heading as="h4" size="sm" marginTop="30px">{children}</Heading>,
-  },
-  list: {
-    bullet: ({ children }) => <Box as="ul" margin={5} >{children}</Box>,
-    number: ({ children }) => <Box as="ol" margin={5} >{children}</Box>,
-  },
-}
 
 export async function getStaticProps({ params }) {
   const client = sanity
