14af763df89558591ffbbf52a3f4b3cae7e4ef0e
DerSchmaik <aiko.zimmermann@gmx.net>
2022-03-01 21:15:27 +0100

    New Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,8 +6,10 @@ export default function Footer() {
     return (
         <Box width="100%">
             <Center>
-                <Text fontSize="sm" color="gray.500" marginTop="60px" marginBottom="30px" textAlign="center">
-                    <p>Made with ❤️ by Aiko using <Link href="https://nextjs.org/">Next.js</Link> and <Link href="https://chakra-ui.com/">Chakra-UI</Link></p>
+                <Text fontSize="sm" color="gray.500" margin="30px" textAlign="center">
+                    <p>Made with ❤️ by Aiko </p>
+                    <p>Using <Link href="https://nextjs.org/">Next.js</Link>, <Link href="https://chakra-ui.com/">Chakra-UI</Link> and <Link href="https://www.sanity.io/">Sanity</Link></p>
+                    <p>Deployed on <Link href="https://vercel.com">Vercel</Link></p>
                 </Text>
             </Center>
         </Box>
